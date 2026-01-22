e43f4fa387521c42c50f67bf1fa672dc228a4377
Nakul Saini <kanhasaini2017@gmail.com>
2026-01-22 21:04:26 +0530

    remove unused files and code from js-server and task directories

--- classwork/js-server/path.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-console.log(`File Name: ${path .basename(__filename)}`);
-console.log(`Directory Name: ${path .dirname(__filename)}`);
-console.log(`Extension: ${path .extname(__filename)}`);
-
-
-const fullpath = path .join(__dirname, 'public', 'index.html');
-console.log(`Full Path: ${fullpath}`);
